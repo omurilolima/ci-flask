8da9ff142760fe4d67274f94759e1ef27ce49b42
Murilo Lima <msl@cesar.school>
2023-02-27 21:03:13 +0000

    Add Template Inheritance

--- run.py
+++ run.py
@@ -13,11 +13,16 @@ def index():
 def about():
     return render_template('about.html')
 
-
+ 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
 
+
+@app.route('/careers')
+def careers():
+    return render_template('careers.html')
+    
 
 if __name__ == '__main__':
     app.run(
